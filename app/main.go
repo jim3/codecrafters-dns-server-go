9bcc475ff24118437a63630cc368d1dce563e290
jim3 <jimmy3.xyz@gmail.com>
2024-11-17 17:15:11 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,13 +5,36 @@ import (
 	"net"
 )
 
-// Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
-var _ = net.ListenUDP
+// Header section structure
+type DnsHeader struct {
+	ID      uint16 // 2 bytes
+	QR      uint8  // 1 bit
+	OPCODE  uint8
+	AA      uint8
+	TC      uint8
+	RD      uint8
+	RA      uint8
+	Z       uint8
+	RCODE   uint8
+	QDCOUNT uint16
+	ANCOUNT uint16
+	NSCOUNT uint16
+	ARCOUNT uint16
+}
+
+func (h *DnsHeader) PacketParser(packet []byte) []byte {
+	data := make([]byte, 12)
+	h.ID = uint16(packet[0])<<8 | uint16(packet[1])
+	h.QR = uint8(packet[2]) << 7
+
+	data[0] = byte(h.ID >> 8)
+	data[1] = byte(h.ID)
+	data[2] = byte(h.QR) // data[2] = 1 << 7
+	return data
+}
 
 func main() {
 	fmt.Println("Starting UDP server...")
-
-	// ResolveUDPAddr returns a *UDPAddr struct
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
 		fmt.Println("Failed to resolve UDP address:", err)
@@ -26,21 +49,18 @@ func main() {
 	defer udpConn.Close()
 
 	buf := make([]byte, 512)
-
 	for {
-		size, source, err := udpConn.ReadFromUDP(buf)
+		// Copies datagram payload into `buf`
+		size, remoteAddr, err := udpConn.ReadFromUDP(buf)
 		if err != nil {
 			fmt.Println("Error receiving data:", err)
 			break
 		}
 
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+		header := &DnsHeader{}
+		r := header.PacketParser(buf[:size])
 
-		// Create an empty response
-		response := []byte{}
-
-		_, err = udpConn.WriteToUDP(response, source)
+		_, err = udpConn.WriteToUDP(r, remoteAddr)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
